329130e271e7ee27b4f56489a89ece0a2ae90a3d
kennydn99 <73307872+kennydn99@users.noreply.github.com>
2023-11-15 15:20:49 -0800

    Create html and js files
    Plan and write pseudocode for first two functions of the game.
    Implement getComputerChoice function and begin implementing playRound

---
+++ app.js
@@ -0,0 +1,34 @@
+console.log("Test!");
+
+// getComputerChoice function
+//     - randomly return either 'Rock', 'Paper', or 'Scissors'
+//
+//  CREATE array/list of choices (rock, paper, scissors)
+//  SET randomNum from 1-3 (3 choices)
+//  RETURN choice from array/list at randomNum index
+function getComputerChoice() {
+    let choices = ["Rock", "Paper", "Scissors"];
+    let randomNum = Math.floor(Math.random() * 3);
+    return choices[randomNum];
+}
+
+// Play single round of game function
+//      - 2 parameters (playerSelection, computerSelection)
+//      - return string declaring winner of round ("You Lose! Paper beats Rock")
+//      - make playerSelection case-insensitive
+//
+//  DECLARE function with parameters
+//      CONVERT playerSelection to lower case
+//      CASE if playerSelection is the same as computerSelection
+//          THEN RETURN tie
+//      CASE if (playerSelection === rock AND computerSelection === paper) OR
+//              (playerSelection === paper AND computerSelection === scissors) OR
+//              (playerSelection === scissors AND computerSelection === rock)
+//          THEN RETURN You Lose computerSelection beats playerSelection
+//      ENDCASE means player wins
+//          RETURN You Win! playerSelection beats computerSelection
+
+function playRound(playerSelection, computerSelection) {
+    let playerChoice = playerSelection.toLowerCase();
+}
+
