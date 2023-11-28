4b06bd9c380b42b9a72481f8d20f7ac9a5b983b3
kennydn99 <73307872+kennydn99@users.noreply.github.com>
2023-11-27 23:43:21 -0800

    Add styling and finishing touches.
    Style the game so it looks less boring.

--- app.js
+++ app.js
@@ -25,17 +25,19 @@ function playRound(playerSelection, computerSelection) {
 function game() {
 
     if (userScore == 5) {
-        display.textContent = `You are the winner of the game! You won ${userScore} times.`;
+        display.textContent = `You are the winner of the game! You won ${userScore} times!`;
     } 
     if (computerScore == 5) {
-        display.textContent = `The computer is the winner of the game! You lost ${computerScore} times.`;
+        display.textContent = `The computer is the winner of the game! You lost ${computerScore} times!`;
     }
 
     if(userScore == 5 || computerScore == 5) {
         btns.style.visibility = 'hidden';
+        
         let playAgainBtn = document.createElement('button');
         playAgainBtn.textContent = 'Play Again';
         display.appendChild(playAgainBtn);
+        
         playAgainBtn.addEventListener('click', (event) => {
             btns.style.visibility = 'visible';
             playAgainBtn.style.visibility = 'hidden';
@@ -43,7 +45,7 @@ function game() {
             userScorePanel.textContent = `Your Score: ${userScore}`;
             computerScore = 0;
             computerScorePanel.textContent = `Computer Score: ${computerScore}`;
-            display.textContent = "";
+            display.textContent = "Let's play Rock, Paper, Scissors. First to 5 gets the W!";
         });
     }
 }
@@ -51,9 +53,9 @@ function game() {
 
 
 let display = document.querySelector('.display');
+display.textContent = "Let's play Rock, Paper, Scissors. First to 5 gets the W!";
 let btns = document.querySelector('.btn-container');
 let scoreboard = document.querySelector('.scoreboard');
-//let playAgainBtn;
 
 let userChoice;
 let userScore = 0;
@@ -85,13 +87,13 @@ btns.addEventListener('click', (event) => {
 
     let outcome = playRound(userChoice, computerChoice);
     if (outcome == 0) {
-        display.textContent = `${userChoice} vs ${computerChoice} : It's a Tie!`;
+        display.textContent = `${userChoice} vs ${computerChoice}: It's a Tie!`;
     } else if (outcome < 0) {
-        display.textContent = `${userChoice} vs ${computerChoice} : Computer wins this round`;
+        display.textContent = `${userChoice} vs ${computerChoice}: Computer wins this round :(`;
         computerScore++;
         computerScorePanel.textContent = `Computer Score: ${computerScore}`;
     } else {
-        display.textContent = `${userChoice} vs ${computerChoice} : You win this round`;
+        display.textContent = `${userChoice} vs ${computerChoice}: You win this round :)`;
         userScore++;
         userScorePanel.textContent = `Your Score: ${userScore}`;
     }
